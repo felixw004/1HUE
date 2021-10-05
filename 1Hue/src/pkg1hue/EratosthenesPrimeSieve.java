58fe6c7c91dd97cdedcc1bde799940bd81fef217
Felix W <WeigertF190039@sus.htl-grieskirchen.com>
2021-10-05 14:13:00 +0200

    second one

---
+++ 1Hue/src/pkg1hue/EratosthenesPrimeSieve.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg1hue;
+
+/**
+ *
+ * @author felix
+ */
+public class EratosthenesPrimeSieve implements PrimeSieve{
+    
+    public EratosthenesPrimeSieve()
+    {
+        
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+    }
+    
+}
