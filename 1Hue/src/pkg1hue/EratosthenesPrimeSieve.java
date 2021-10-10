9041e40ee8e1f75451cacd9d3c73a1b0fd2261be
WeigertF <WeigertF190039@sus.htl-grieskirchen.at>
2021-10-10 12:59:30 +0200

    second home

--- 1Hue/src/pkg1hue/EratosthenesPrimeSieve.java
+++ 1Hue/src/pkg1hue/EratosthenesPrimeSieve.java
@@ -8,39 +8,41 @@ package pkg1hue;
  *
  * @author felix
  */
-public class EratosthenesPrimeSieve implements PrimeSieve{
-    
+public class EratosthenesPrimeSieve implements PrimeSieve {
+
     int obergrenze;
-    
-    public EratosthenesPrimeSieve(int obergrenze)
-    {
+
+    public EratosthenesPrimeSieve(int obergrenze) {
         this.obergrenze = obergrenze;
     }
 
     @Override
     public boolean isPrime(int p) {
         //Math sqrt = Quadratwurzel
-        
-         { if (p <= 1) {
-        return false; }
-        for (int i = 2; i <= Math. sqrt(p); i++) { if (p % i == 0) {
-            return false; }
-        } return true;
+
+        {
+            if (p <= 1) {
+                return false;
+            }
+            for (int i = 2; i <= Math.sqrt(p); i++) {
+                if (p % i == 0) {
+                    return false;
+                }
+            }
+            return true;
         }
-        }
-    
+    }
 
     @Override
     public void printPrimes(int[] l) {
-        
-        for( int i = 2; i < l.length; i++)
-        {
-            if(l[i]==1) {
+
+        for (int i = 2; i < l.length; i++) {
+            if (l[i] == 1) {
                 System.out.println(i + " ");
             }
         }
         System.out.println();
-        
+
     }
-    
+
 }
--- 1Hue/src/pkg1hue/Main.java
+++ 1Hue/src/pkg1hue/Main.java
@@ -4,6 +4,7 @@
  */
 package pkg1hue;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -16,66 +17,91 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         System.out.println("start");
         System.out.println();
         System.out.println("obergrenze eingeben");
-        
+
         Scanner sc = new Scanner(System.in);
         int obergrenze = sc.nextInt();
-        
-        int [] zahlen = legeArrayAn(obergrenze);
+
+        int[] zahlen = legeArrayAn(obergrenze);
         bereiteArrayVor(zahlen);
         pruefeIndex(zahlen);
-        
+
         EratosthenesPrimeSieve e = new EratosthenesPrimeSieve(obergrenze);
         e.printPrimes(zahlen);
-     
-        
-        
+
+        ArrayList<Integer> geradenbisOG = new ArrayList<Integer>();
+        geradenbisOG = allegeradenbisobergrenze(10);
+        primzahlenbisobergrenze(10);
+
     }
-    
-    public static int[] legeArrayAn(int n)
-    {
+
+    public static int[] legeArrayAn(int n) {
         int[] meinArray = new int[n + 1];
-        
+
         return meinArray;
     }
-    
+
     public static void bereiteArrayVor(int[] a) {
-        
+
         for (int i = 2; i < a.length; i++) {
-            
-        a[i] = 1;
+
+            a[i] = 1;
+        }
     }
-    }
-    
+
     public static void pruefeIndex(int[] liste) {
-        
+
         for (int i = 2; i < liste.length; i++) {
-            if(liste[i] == 1)
-            {
+            if (liste[i] == 1) {
                 entferneVielfacheVon(liste, i);
             }
         }
     }
-    
-    public static void entferneVielfacheVon(int[] liste, int faktor)
-    {
+
+    public static void entferneVielfacheVon(int[] liste, int faktor) {
         int i = 2;
-        
-        while ( i* faktor < liste.length) {
-            
-            if(liste[i * faktor] == 1) {
-                
+
+        while (i * faktor < liste.length) {
+
+            if (liste[i * faktor] == 1) {
+
                 liste[i * faktor] = 0;
             }
-            
+
             i++;
         }
     }
-   
-    
-    
-    
+
+    public static ArrayList<Integer> allegeradenbisobergrenze(int obergrenze2) {
+        int erg = 3;
+        ArrayList<Integer> geraden = new ArrayList<Integer>();
+
+        while (erg <= obergrenze2) {
+            if (erg % 2 == 0) {
+                geraden.add(erg);
+            }
+            erg++;
+        }
+        return geraden;
+    }
+
+    public static void primzahlenbisobergrenze(int obergrenze2) {
+
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        EratosthenesPrimeSieve e1 = new EratosthenesPrimeSieve(obergrenze2);
+
+        for (int i = 0; i <= obergrenze2; i++) {
+
+            if (e1.isPrime(i) == true) {
+                primes.add(i);
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
 }
